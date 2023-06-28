82b6f8d6b67fafeeb853b3bf1ce1696213cbde31
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-28 17:45:32 +0300

    Add Interests

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,6 +1,19 @@
+import styles from '../styles/Interests.module.css';
+
 const Interests = () => {
   return (
-    <div>Interests</div>
+    <section className={styles.container}>
+      <h2>interests</h2>
+      <p>Some things I enjoy learning about & doing</p>
+      <div>
+        <span>SOFTWARE</span>
+        <span>EXPLORING THINGS</span>
+        <span>BUILDING PROJECTS</span>
+        <span>MUSIC 💕</span>
+        <span>MOVIES</span>
+        <span>FOOTBALL</span>
+      </div>
+    </section>
   )
 }
 
