ca99df09bb85ec55e551ffb7796bb08cd132b4ed
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-29 01:49:53 +0300

    Add Stylings to Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About'
+import Contact from './components/Contact'
 import Header from './components/Header'
 import Interests from './components/Interests'
 import Navbar from './components/Navbar'
@@ -13,6 +14,7 @@ function App() {
       <About /> 
       <Interests />
       <Projects />
+      <Contact />
     </>
   )
 }
