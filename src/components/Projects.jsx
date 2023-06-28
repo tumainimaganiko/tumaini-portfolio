779bbf10c20bd7098b456fbae91f3fc07a84520e
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-28 20:12:33 +0300

    Import ProjectsDetails Component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,14 @@
+import ProjectsDetails from "./ProjectsDetails"
+
 const Projects = () => {
+  const container = {
+    margin: '0 5%',
+  }
   return (
-    <section>
+    <section style={container}>
       <h2>projects</h2>
       <p>some things I've worked (am working) on</p>
+      <ProjectsDetails />
     </section>
   )
 }
