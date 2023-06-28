7b5b63f386dd6d1ad1e5430658302dc0b320ad24
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-28 14:12:03 +0300

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+      <h2>Hello!</h2>
+      <p>
+        I'm Tumaini, and I currently attend Microverse for Full-stack
+        development studies. Welcome to my first web dev project 😎
+      </p>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
