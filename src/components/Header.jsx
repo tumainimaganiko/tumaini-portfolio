20aa7ad71342e50f073a4717b8f7048d378facbe
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-28 16:45:59 +0300

    Add Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaAngellist } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
+import styles from '../styles/Header.module.css';
+
 const Header = () => {
   return (
-    <div>
-      <h2>Hello!</h2>
+    <section className={styles.container}>
+      <h2>hello!</h2>
       <p>
-        I'm Tumaini, and I currently attend Microverse for Full-stack
-        development studies. Welcome to my first web dev project 😎
+        I&apos;m Tumaini, and I currently attend Microverse for Full-stack
+        development. Welcome to my first web dev project 😎
       </p>
-      <div></div>
-    </div>
+      <div className={styles.details}>
+        <div>
+            <a href="https://www.linkedin.com/in/tumainimaganiko/" target="_blank"><FaLinkedin className={styles.logo} /></a>
+            <a href="https://github.com/tumainimaganiko" target="_blank"><BsGithub className={styles.logo} /></a>
+            <a href="https://wellfound.com/u/tumaini-maganiko" target="_blank"><FaAngellist className={styles.logo} /></a>
+            <a href="https://twitter.com/Chief2maini" target="_blank"><FaTwitter className={styles.logo} /></a>
+        </div>
+        <div>
+        <a href="https://github.com/tumainimaganiko" target="_blank">VIEW PROJECTS</a>
+        <a href="https://github.com/tumainimaganiko" target="_blank">GET IN TOUCH</a>
+        </div>
+      </div>
+    </section>
   );
 };
 
