6fe77c2f8c49c2236032d2b44fc42acd48497c65
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-29 01:49:28 +0300

    Create Contact Component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,27 @@
+import { BiLogoGmail } from "react-icons/bi";
+import { FaLinkedin } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
+import { FaAngellist } from "react-icons/fa";
+import styles from "../styles/Contact.module.css";
+
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className={styles.container}>
+      <h2>get in touch</h2>
+      <p>
+        I'm currently open to new opportunities, inbox is always open! Find me
+        everywhere @tumainimaganiko
+      </p>
+      <div>
+        <BiLogoGmail />
+        <FaLinkedin />
+        <BsGithub />
+        <FaAngellist />
+        <FaTwitter />
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
