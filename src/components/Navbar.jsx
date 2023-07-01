ea4e440841ae0e05fc97b2002d9a0064b2995080
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-01 12:57:58 +0300

    Update Nv Border

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between text-center p-4 border border-slate-800 items-center">
+    <nav className="flex justify-between text-center p-4 border-b border-[#272D2B] items-center">
       <h1 className="text-3xl italic tablet:text-4xl">Tumaini Maganiko</h1>
       <GiHamburgerMenu className="text-2xl tablet:text-4xl laptop:hidden" />
       <div className="hidden laptop:block">
