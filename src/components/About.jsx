03697cde6cfaf37a41b45899a92c2057f14bbdae
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-01 17:02:14 +0300

    Modify Vertical alignment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
 const About = () => {
   return (
-    <section id="about" className="my-[25%] mx-[5%] text-white tablet:flex tablet:mx-[5%] tablet:my-0">
-      <h2 className="text-center text-[28px] italic tablet:text-left whitespace-nowrap">
+    <section id="about" className="my-[25%] mx-[5%] text-white tablet:flex tablet:mx-[5%] tablet:my-0 laptop:w-[60%] laptop:my-0 laptop:mx-auto">
+      <h2 className="text-center text-[28px] laptop:text-3xl italic tablet:text-left whitespace-nowrap ">
         about me
       </h2>
-      <div className="m-[2%]">
-        <p className="text-justify mb-2">
+      <div className="m-[2%] laptop:my-1">
+        <p className="text-justify mb-2 ">
           Welcome to my portfolio! I&apos;m Tumaini Maganiko, a dedicated
           full-stack software developer with a passion for crafting innovative
           digital solutions. From front-end design to back-end development, I
