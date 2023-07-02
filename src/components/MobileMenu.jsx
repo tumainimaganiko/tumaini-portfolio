ea39b7146aba87e561397545c2f049d3e18e2e82
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-02 13:15:47 +0300

    Create Overlay Window

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -3,7 +3,8 @@ import { HiXMark } from "react-icons/hi2";
 const MobileMenu = ({ display, updateDisplay }) => {
   return (
     display && (
-        <div className="bg-blue-600 fixed w-[90%] mx-[5%] h-[90%] top-[5%] bottom-[5%]">
+      <div className="fixed left-0 right-0 top-0 bottom-0 bg-black opacity-80 z-20 backdrop-blur-lg">
+      <div className="bg-blue-600 fixed w-[90%] mx-[5%] h-[90%] top-[5%] bottom-[5%]">
           <button onClick={() => updateDisplay(false)}>
             <HiXMark />
           </button>
@@ -19,7 +20,7 @@ const MobileMenu = ({ display, updateDisplay }) => {
             </li>
           </ul>
         </div>
-    //   </div>
+       </div>
     )
   );
 };
