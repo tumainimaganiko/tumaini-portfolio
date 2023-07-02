f7db71bf8741e66e085514d97fa4330642a93cc2
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-02 13:16:50 +0300

    Create State to Display Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
+  const [display, setDisplay ] = useState(false);
+  const updateDisplay = (view) => {
+    setDisplay(view)
+  }
   return (
-    <nav className="flex justify-between text-center p-4 border-b border-[#272D2B] items-center">
-      <h1 className="text-3xl italic tablet:text-4xl">Tumaini Maganiko</h1>
-      <GiHamburgerMenu className="text-2xl tablet:text-4xl laptop:hidden" />
-      <div className="hidden laptop:block">
-        <a href="#about" className="p-2 border m-3 rounded-3xl text-xs hover:bg-[#2F3634] border-[#2F3634]">ABOUT</a>
-        <a href="#project" className="p-2 border m-3 rounded-3xl text-xs hover:bg-[#2F3634] border-[#2F3634]">PROJECTS</a>
-        <a href="#contact" className="p-2 border m-3 rounded-3xl text-xs hover:bg-[#2F3634] border-[#2F3634]">GET IN TOUCH</a>
-      </div>
-    </nav>
+    <>
+      {!display && (
+        <nav className="flex justify-between text-center p-4 border-b border-[#272D2B] items-center">
+        <h1 className="text-3xl italic tablet:text-4xl">Tumaini Maganiko</h1>
+        <button onClick={() => setDisplay(true)}>
+        <GiHamburgerMenu className="text-2xl tablet:text-4xl laptop:hidden" />
+        </button>
+        <div className="hidden laptop:block">
+          <a href="#about" className="p-2 border m-3 rounded-3xl text-xs hover:bg-[#2F3634] border-[#2F3634]">ABOUT</a>
+          <a href="#project" className="p-2 border m-3 rounded-3xl text-xs hover:bg-[#2F3634] border-[#2F3634]">PROJECTS</a>
+          <a href="#contact" className="p-2 border m-3 rounded-3xl text-xs hover:bg-[#2F3634] border-[#2F3634]">GET IN TOUCH</a>
+        </div>
+      </nav>
+      )}
+    <MobileMenu display={display} updateDisplay={updateDisplay}/>
+    </>
   );
 };
 
