933a576549556164ae24f36ee36e89cbf40fdec1
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-03 21:56:02 +0300

    Create Popup Component

---
+++ src/components/Popup.jsx
@@ -0,0 +1,13 @@
+import { HiXMark } from "react-icons/hi2";
+import { useSelector } from "react-redux";
+
+const Popup = ({popup, updatePopup,id}) => {
+    const { projects } = useSelector(store => store.projects)
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Popup;
