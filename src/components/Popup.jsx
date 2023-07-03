89a0c56bbd9dbd0c39858da02f5b9141e3b9c173
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-03 21:56:59 +0300

    Create Popup Elements

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -5,7 +5,16 @@ const Popup = ({popup, updatePopup,id}) => {
     const { projects } = useSelector(store => store.projects)
   return (
     <>
-        
+        {popup && (
+            <div className="fixed top-0 left-0 right-0 bottom-0 bg-black">
+              <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-3/4 w-1/2 border border-red-600 bg-white text-black">
+              <button onClick={() => updatePopup(false)} className="fixed right-1 top-1">
+                <HiXMark />
+              </button>
+              <p>{projects[id].name}</p>
+            </div>
+            </div>
+          )}
     </>
   )
 }
