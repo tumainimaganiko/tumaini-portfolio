5a450ca6fec85b09e6768adedcd571f1eeedd071
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-04 13:24:28 +0300

    Update Hovering Effect

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -3,11 +3,12 @@ import { useSelector } from "react-redux";
 
 const Popup = ({ popup, updatePopup, id }) => {
   const { projects } = useSelector((store) => store.projects);
+
   return (
     <>
       {popup && (
         <>
-          <div className="fixed left-[50%] w-[90%] translate-x-[-50%] top-[50%] translate-y-[-50%] laptop:h-3/4 laptop:w-1/2 border border-red-600 opacity-100 bg-white text-black z-[1000]">
+          <div className="fixed left-[50%] w-[90%] translate-x-[-50%] top-[50%] translate-y-[-50%] laptop:h laptop:w-[70%] border border-red-600 opacity-100 bg-white text-black z-[1000]">
             <button
               onClick={() => updatePopup(false)}
               className="absolute laptop:fixed right-1 top-1"
@@ -15,16 +16,45 @@ const Popup = ({ popup, updatePopup, id }) => {
               <HiXMark />
             </button>
             <div className="p-3">
-              <h2 className="text-center font-bold">
-              {projects[id].name}
-              </h2>
+              <h2 className="text-center font-bold">{projects[id].name}</h2>
               <img className="w-full h-1/2" src={projects[id].image} alt="" />
-              <p className="text-justify">
-              {projects[id].description}
-              </p>
+              <p className="text-justify">{projects[id].description}</p>
               <div className="flex justify-between">
-                <a className="px-4 bg-[#272D2B] border border-[#272D2B] rounded-3xl text-xs text-white py-2" href={projects[id].liveLink} target="_blank">SEE LIVE</a>
-                <a className="px-4 bg-[#272D2B] border border-[#272D2B] rounded-3xl text-xs text-white py-2" href={projects[id].githubLink} target="_blank">SEE SOURCE</a>
+                {id === 3 ? (
+                  <>
+                    <a
+                      className="px-4 bg-[#272D2B] border border-[#272D2B]laptop:bg-[black] hover:laptop:bg-[bisque]  rounded-3xl text-xs text-white hover:text-black hover:border-black py-2"
+                      href={projects[id].liveLink}
+                      target="_blank"
+                    >
+                      SEE LIVE
+                    </a>
+                    <a
+                      className="px-4 bg-[#272D2B] border border-[#272D2B]laptop:bg-[black] hover:laptop:bg-[bisque]  rounded-3xl text-xs text-white hover:text-black hover:border-black py-2"
+                      href={projects[id].githubLink}
+                      target="_blank"
+                    >
+                      SEE SOURCE
+                    </a>
+                  </>
+                ) : (
+                  <>
+                    <a
+                      className="px-4 bg-[#272D2B] border border-[#272D2B]laptop:bg-[black] hover:laptop:bg-blue-500  rounded-3xl text-xs text-white py-2"
+                      href={projects[id].liveLink}
+                      target="_blank"
+                    >
+                      SEE LIVE
+                    </a>
+                    <a
+                      className="px-4 bg-[#272D2B] border border-[#272D2B]laptop:bg-[black] hover:laptop:bg-blue-500  rounded-3xl text-xs text-white py-2"
+                      href={projects[id].githubLink}
+                      target="_blank"
+                    >
+                      SEE SOURCE
+                    </a>
+                  </>
+                )}
               </div>
             </div>
           </div>
