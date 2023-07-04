f19983dabadedad8b09149320c391aa71afc519c
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-04 11:07:23 +0300

    Style Mobile Popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -7,22 +7,24 @@ const Popup = ({ popup, updatePopup, id }) => {
     <>
       {popup && (
         <>
-          <div className="fixed w-[90%] h-[80%] left-[5%] right-[5%] top-[10%] bottom-[10%] laptop:top-[50%] laptop:left-[50%] laptop:translate-x-[-50%] laptop:translate-y-[-50%] laptop:h-3/4 laptop:w-1/2 border border-red-600 opacity-100 bg-white text-black z-[1000]">
+          <div className="fixed left-[50%] w-[90%] translate-x-[-50%] top-[50%] translate-y-[-50%] laptop:h-3/4 laptop:w-1/2 border border-red-600 opacity-100 bg-white text-black z-[1000]">
             <button
               onClick={() => updatePopup(false)}
               className="absolute laptop:fixed right-1 top-1"
             >
               <HiXMark />
             </button>
-            <div>
-              <h2 className="text-center">
+            <div className="p-3">
+              <h2 className="text-center font-bold">
               {projects[id].name}
               </h2>
-              <p>{projects[id].description}</p>
-              <img src={projects[id].image} alt="" />
-              <div>
-                <a href={projects[id].liveLink}>SEE LIVE</a>
-                <a href={projects[id].githubLink}>SEE SOURCE</a>
+              <img className="w-full h-1/2" src={projects[id].image} alt="" />
+              <p className="">
+              {projects[id].description}
+              </p>
+              <div className="flex justify-between">
+                <a className="px-4 bg-[#272D2B] text-white py-2" href={projects[id].liveLink} target="_blank">SEE LIVE</a>
+                <a className="px-4 bg-[#272D2B] text-white py-2" href={projects[id].githubLink} target="_blank">SEE SOURCE</a>
               </div>
             </div>
           </div>
