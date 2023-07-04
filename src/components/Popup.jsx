269ccc545ab810f1e2ae1ccaa8f67dcc0fbf3f27
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-04 09:58:42 +0300

    Add Buttons

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -7,17 +7,23 @@ const Popup = ({ popup, updatePopup, id }) => {
     <>
       {popup && (
         <>
-          <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-3/4 w-1/2 border border-red-600 opacity-100 bg-white text-black z-[1000]">
+          <div className="fixed w-[90%] h-[80%] left-[5%] right-[5%] top-[10%] bottom-[10%] laptop:top-[50%] laptop:left-[50%] laptop:translate-x-[-50%] laptop:translate-y-[-50%] laptop:h-3/4 laptop:w-1/2 border border-red-600 opacity-100 bg-white text-black z-[1000]">
             <button
               onClick={() => updatePopup(false)}
-              className="fixed right-1 top-1"
+              className="absolute laptop:fixed right-1 top-1"
             >
               <HiXMark />
             </button>
             <div>
-              <h2>{projects[id].name}</h2>
+              <h2 className="text-center">
+              {projects[id].name}
+              </h2>
               <p>{projects[id].description}</p>
               <img src={projects[id].image} alt="" />
+              <div>
+                <a href={projects[id].liveLink}>SEE LIVE</a>
+                <a href={projects[id].githubLink}>SEE SOURCE</a>
+              </div>
             </div>
           </div>
           <div className="fixed top-0 left-0 right-0 bottom-0 bg-black/10 z-[800] backdrop-blur-sm"></div>
