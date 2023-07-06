945900af7390aa207167ed742db76336e333ef7e
tumainimaganiko <tumainimaganiko@gmail.com>
2023-07-06 07:48:19 +0300

    Update Fonts

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,10 +3,10 @@ import ProjectsDetails from "./ProjectsDetails";
 const Projects = () => {
   return (
     <section id="project" className="my-0 mx-[5%] desktop:mx-[10%]">
-      <h2 className="text-center font-normal leading-normal italic text-[28px]">
+      <h2 className="text-center font-[mazius] font-normal leading-normal italic text-[28px]">
         projects
       </h2>
-      <p className="text-center font-normal leading-normal">
+      <p className="text-center font-[avenir] font-normal leading-normal">
         some things I&apos;ve worked (am working) on
       </p>
       <ProjectsDetails />
