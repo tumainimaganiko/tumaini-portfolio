ee9f733dc46bcc6784f23597782fb82b9920fa5b
tumainimaganiko <tumainimaganiko@gmail.com>
2023-10-10 03:16:05 +0300

    Update Dark Mode Preferences

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { BsGithub } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <section className="bg-white text-black rounded-3xl m-5 p-5 tablet:my-[10%] tablet:mx-[5%] laptop:p-16 desktop:mx-[10%]">
+    <section className="bg-slate-800 text-white dark:bg-white dark:text-black rounded-3xl m-5 p-5 tablet:my-[10%] tablet:mx-[5%] laptop:p-16 desktop:mx-[10%] ">
       <h2 className="text-3xl italic mt-0 font-[mazius]">hello!</h2>
       <p className="text-base text-justify leading-5 tablet:text-left font-[avenir] font-normal">
         I am a software developer working productively in dynamic environments.
@@ -20,7 +20,7 @@ const Header = () => {
             rel="noreferrer"
             className="p-0 mx-2"
           >
-            <FaLinkedin className="text-2xl text-black" />
+            <FaLinkedin className="text-2xl text-white dark:text-black" />
           </a>
           <a
             href="https://github.com/tumainimaganiko"
@@ -28,7 +28,7 @@ const Header = () => {
             rel="noreferrer"
             className="p-0 mx-2"
           >
-            <BsGithub className="text-2xl text-black" />
+            <BsGithub className="text-2xl text-white dark:text-black" />
           </a>
           <a
             href="https://wellfound.com/u/tumaini-maganiko"
@@ -36,7 +36,7 @@ const Header = () => {
             rel="noreferrer"
             className="p-0 mx-2"
           >
-            <FaAngellist className="text-2xl text-black" />
+            <FaAngellist className="text-2xl text-white dark:text-black" />
           </a>
           <a
             href="https://twitter.com/Chief2maini"
@@ -44,19 +44,19 @@ const Header = () => {
             rel="noreferrer"
             className="p-0 mx-2"
           >
-            <FaTwitter className="text-2xl text-black" />
+            <FaTwitter className="text-2xl text-white dark:text-black" />
           </a>
         </div>
         <div className="flex flex-col items-center w-[70%] tablet:w-1/2 tablet:justify-between tablet:flex-row laptop:w-[40%] laptop:justify-end">
           <a
             href="#project"
-            className="no-underline py-[5px] px-[15px] border border-slate-950 rounded-[20px] text-slate-950 text-[11px] font-bold m-[2px] w-[165px] text-center hover:bg-[#2F3634] hover:text-white font-[neutralface]"
+            className="no-underline py-[5px] px-[15px] border border-white dark:border-slate-950 rounded-[20px] text-white dark:text-slate-950 text-[11px] font-bold m-[2px] w-[165px] text-center hover:bg-white dark:hover:bg-[#2F3634] hover:text-black dark:hover:text-white font-[neutralface]"
           >
             VIEW PROJECTS
           </a>
           <a
             href="#contact"
-            className="no-underline py-[5px] px-[15px] border border-slate-950 rounded-[20px] text-slate-950 text-[11px] font-bold m-[2px] w-[165px] text-center hover:bg-[#2F3634] hover:text-white font-[neutralface]"
+            className="no-underline py-[5px] px-[15px] border border-white dark:border-slate-950 rounded-[20px] text-white dark:text-slate-950 text-[11px] font-bold m-[2px] w-[165px] text-center hover:bg-white dark:hover:bg-[#2F3634] hover:text-black dark:hover:text-white font-[neutralface]"
           >
             GET IN TOUCH
           </a>
